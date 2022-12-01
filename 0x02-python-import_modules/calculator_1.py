edabb1aba5d9d2679bc2a4cc7df5162b54ee011a
ssginfotech <djssg2022@gmail.com>
2022-12-01 14:58:30 -0800

    my commit

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -38,13 +38,13 @@ def mul(a, b):
     return (a * b)
 
 
+
 def div(a, b):
     """My division function
 
     Args:
         a: first integer
         b: second integer
-
 
     Returns:
         The return value. a / b
