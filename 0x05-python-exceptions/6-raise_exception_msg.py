be56b9909905dc0115da29b7358774eb04dc52c9
Simon Solomon Galadima <112211938+ssginfotech@users.noreply.github.com>
2022-12-20 04:24:04 +0100

    Update 6-raise_exception_msg.py
    raise exception 2

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 6-raise_exception_msg.py
+# Simon Solomon Galadima <375@holbertonschool.com>
+
+
+def raise_exception_msg(message=""):
+    """Raise a NameError exception with a message."""
+    raise NameError(message)
