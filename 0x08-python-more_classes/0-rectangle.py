d510c00d239ff97a3d5f4ce3d1626f3cb1e6282a
ssginfotech <djssg2022@gmail.com>
2023-01-04 03:55:11 -0800

    my commit

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-rectangle.py
+# Simon Solomon Galadima <375@holbertonschool.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+    pass
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 1-rectangle.py
+# Simon Solomon Galadima <375@holbertonschool.com>
+"""Defines a Rectangle class."""
+
+
+class Rectangle:
+    
